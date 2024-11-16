6552b4a9e3d905746de317eca43bb454d0d5cf2a
Etienne Rivard <etienne@kerzo.ca>
2024-11-16 09:27:31 -0500

    retroaction

---
+++ correction.md
@@ -0,0 +1,6 @@
+Voici ce qui est absent du jeu :
+
+- Le bouton relancer ne relance pas le jeu;
+- Une seule carte est retournée à la fois, alors que 2 sont nécessaires pour jouer;
+- Les cartes ne sont pas mélangées;
+- Les cartes ne sont retournées face cachée après un certain 1 seconde;
--- src/cartes.tsx
+++ src/cartes.tsx
@@ -1,69 +1,71 @@
-import { Button, Container } from "@mui/material";
-import { useState } from "react";
+import { Button, Container } from '@mui/material';
+import { useState } from 'react';
 type Carte = {
-    id:number;
-    image:string;
-    estRetourne:boolean;
-  }
+  id: number;
+  image: string;
+  estRetourne: boolean;
+};
 interface CartesProps {
-    cartes:Carte[],
-    essais:number,
-    setEssais: (nombre:number) => void
+  cartes: Carte[];
+  essais: number;
+  setEssais: (nombre: number) => void;
 }
-function Cartes(props:CartesProps)
-{
-    const [cartePrecedente, setCartePrecedente] = useState(-1)
-    const [posCarte,setPosCarte] = useState(-1)
-    
-    function RetournerCarte(idCarte:number,position:number){
-        props.cartes[position].estRetourne = true
-        if (cartePrecedente == -1){
-            setCartePrecedente(idCarte)
-            setPosCarte(position)
-        }
-        else{      
-            if (cartePrecedente == idCarte){
-                //Points++
-            }
-            else{
-                setTimeout(() => {
-                    props.cartes[position].estRetourne = false
-                    props.cartes[posCarte].estRetourne = false 
-                    setCartePrecedente(-1)
-                    setPosCarte(-1)
-                    props.setEssais(props.essais - 1)  
-                }
-                ,500);
-                
-            }
-            
-        }
+function Cartes(props: CartesProps) {
+  const [cartePrecedente, setCartePrecedente] = useState(-1);
+  // ER: nom de variable pas clair... Que voulais-tu dire par posCarte?
+  const [posCarte, setPosCarte] = useState(-1);
+
+  function RetournerCarte(idCarte: number, position: number) {
+    props.cartes[position].estRetourne = true;
+    if (cartePrecedente == -1) {
+      setCartePrecedente(idCarte);
+      setPosCarte(position);
+    } else {
+      if (cartePrecedente == idCarte) {
+        //Points++
+      } else {
+        setTimeout(() => {
+          props.cartes[position].estRetourne = false;
+          props.cartes[posCarte].estRetourne = false;
+          setCartePrecedente(-1);
+          setPosCarte(-1);
+          props.setEssais(props.essais - 1);
+        }, 500);
+      }
     }
-    
-    
-    const AfficherCartes = props.cartes.map((carte,indexCarte) => 
-            <Button sx={{
-                height:100,
-                width:100,
-            }}
-            onClick={() => RetournerCarte(carte.id,indexCarte)}>
-                <img src={carte.estRetourne? carte.image:"/dessus-carte.svg"} alt="" height={100} width={100}/>
-            </Button>);
-    
-    return(
-    
+  }
+
+  const AfficherCartes = props.cartes.map((carte, indexCarte) => (
+    <Button
+      sx={{
+        height: 100,
+        width: 100,
+      }}
+      onClick={() => RetournerCarte(carte.id, indexCarte)}
+    >
+      <img
+        src={carte.estRetourne ? carte.image : '/dessus-carte.svg'}
+        alt=""
+        height={100}
+        width={100}
+      />
+    </Button>
+  ));
+
+  return (
     <>
-        <Container
-            sx={{
-                display:'flex',
-                flexDirection:'row',
-                flexWrap:'wrap',
-                width: 450
-            }}
-        >
-            {AfficherCartes}
-        </Container>
+      <Container
+        sx={{
+          display: 'flex',
+          flexDirection: 'row',
+          flexWrap: 'wrap',
+          width: 450,
+        }}
+      >
+        {AfficherCartes}
+      </Container>
     </>
-)}
+  );
+}
 
 export default Cartes;
